7523584cf763cdd524594439da721460f11ba524
zenithshrestha37 <zenithshrestha37@gmail.com>
2021-08-17 09:38:39 -0500

    schemas and GraphQl

--- src/main/java/com/pluralsight/entity/Application.java
+++ src/main/java/com/pluralsight/entity/Application.java
@@ -26,6 +26,7 @@ public class Application {
         this.owner = owner;
         this.description = description;
     }
+
     public Long getId() {
         return id;
     }
--- src/main/java/com/pluralsight/exception/ApplicationNotFoundException.java
+++ src/main/java/com/pluralsight/exception/ApplicationNotFoundException.java
@@ -1,8 +1,34 @@
 package com.pluralsight.exception;
 
-public class ApplicationNotFoundException extends RuntimeException {
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
 
-    public ApplicationNotFoundException(String exception) {
-        super(exception);
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ApplicationNotFoundException extends RuntimeException implements GraphQLError {
+
+    private Map<String, Object> extensions = new HashMap<>();
+
+    public ApplicationNotFoundException(String message, Long invalidApplicationId) {
+        super(message);
+        extensions.put("invalidApplicationId", invalidApplicationId);
+    }
+
+    @Override
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getExtensions() {
+        return extensions;
+    }
+
+    @Override
+    public ErrorType getErrorType() {
+        return ErrorType.DataFetchingException;
     }
 }
